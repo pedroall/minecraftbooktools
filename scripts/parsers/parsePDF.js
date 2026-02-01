2b59fcc28adff8bd0e68efe5c1a724498c02bae5
pedroall <cttpedroall@gmail.com>
2026-02-01 04:04:50 -0300

    fix github bugs

--- scripts/parsers/parsePDF.js
+++ scripts/parsers/parsePDF.js
@@ -1,17 +1,13 @@
-import * as pdfjs from '/pdfjs/pdf.mjs';
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-    "./pdfjs/pdf.worker.mjs";
+pdfjsLib.GlobalWorkerOptions.workerSrc = '../../pdfjs/pdf.worker.mjs';
 export async function parsePDF(file) {
     const buffer = await file.arrayBuffer();
-    const pdf = await pdfjs.getDocument({ data: buffer }).promise;
-    let text = "";
+    const pdf = await pdfjsLib.getDocument({ data: buffer }).promise;
+    let text = '';
     for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
         const page = await pdf.getPage(pageNumber);
         const content = await page.getTextContent();
-        const pageText = content.items
-            .map(item => item.str)
-            .join(" ");
-        text += pageText + "\n\n";
+        const pageText = content.items.map((item) => item.str).join(' ');
+        text += pageText + '\n\n';
     }
     return text;
 }
